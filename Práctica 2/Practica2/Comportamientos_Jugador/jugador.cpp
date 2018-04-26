44a05a7338200752d49ea1faaaaba310b79eeb27
patchispatch <juanov98@gmail.com>
2018-04-26 17:56:29 +0200

    Implementado paso a plan, preguntar a Rocío por contenedor abiertos

--- Práctica 2/Practica2/Comportamientos_Jugador/jugador.cpp
+++ Práctica 2/Practica2/Comportamientos_Jugador/jugador.cpp
@@ -5,7 +5,22 @@
 #include <iostream>
 #include <cmath>
 #include <list>
-#include <set>
+
+
+bool ordenar (const estado &uno, const estado &otro) {
+
+	return uno.coste_f < otro.coste_f;
+}
+
+bool operator== (const estado &uno, const estado &otro) {
+
+	return uno.fila == otro.fila && uno.columna == otro.columna;
+}
+
+bool operator!= (const estado &uno, const estado &otro) {
+
+	return !(uno == otro);
+}
 
 void ComportamientoJugador::PintaPlan(list<Action> plan) {
 	auto it = plan.begin();
@@ -62,16 +77,24 @@ void ComportamientoJugador::VisualizaPlan(const estado &st, const list<Action> &
 
 
 bool ComportamientoJugador::pathFinding(const estado &origen, const estado &destino, list<Action> &plan) {
-	set<estado> abiertos;
-	set<estado> cerrados;
-	set<estado> ruta;
+	list<estado> abiertos;
+	vector<vector<bool>> cerrados;
+
+	for (int i = 0; i < mapaResultado.size(); ++i) {
+		for (int j = 0; i < mapaResultado.size(); ++j) {
+			mapaResultado[i][j] = false;
+		}
+	}
+
+	list<estado> ruta;
 
 	estado e1, e2, e3, e4;
-	origen.coste_g = 0;
 	estado actual = origen;
 
-	while (!(cerrados.count(destino))) {
-		cerrados.push_back(actual);
+	actual.coste_g = 0;
+
+	while (!(cerrados[destino.fila][destino.columna])) {
+		cerrados[actual.fila][actual.columna] = true;
 		abiertos.erase(actual); 
 
 		e1 = actual; 
@@ -84,51 +107,144 @@ bool ComportamientoJugador::pathFinding(const estado &origen, const estado &dest
 		++e3.columna;
 		--e4.columna;
 
-		e1.padre = actual;
-		e2,padre = actual;
-		e3.padre = actual;
-		e4.padre = actual;
+		e1.padre = &actual;
+		e2.padre = &actual;
+		e3.padre = &actual;
+		e4.padre = &actual;
 
 
-		if(mapaResultado[e1.fila][e1.columna] != 'S' && mapaResultado[e1.fila][e1.columna] != 'T') {
-			cerrados.push_back(e1);
+		if(mapaResultado[e1.fila][e1.columna] != 'S' && mapaResultado[e1.fila][e1.columna] != 'T' && mapaResultado[e1.fila][e1.columna] != 'K') {
+			cerrados[e1.fila][e1.columna] = true;
 		}
 		else {
 			abiertos.push_back(e1);
 			e1.coste_f = g(e1) + h(e1);
 		}
 
-		if(mapaResultado[e2.fila][e2.columna] != 'S' && mapaResultado[e2.fila][e2.columna] != 'T') {
-			cerrados.push_back(e2);
+		if(mapaResultado[e2.fila][e2.columna] != 'S' && mapaResultado[e2.fila][e2.columna] != 'T' && mapaResultado[e2.fila][e2.columna] != 'K') {
+			cerrados[e2.fila][e2.columna] = true;
 		}
 		else {
 			abiertos.push_back(e2);
 			e1.coste_f = g(e2) + h(e2);
 		}
 
-		if(mapaResultado[e3.fila][e3.columna] != 'S' && mapaResultado[e3.fila][e3.columna] != 'T') {
-			cerrados.push_back(e3);
+		if(mapaResultado[e3.fila][e3.columna] != 'S' && mapaResultado[e3.fila][e3.columna] != 'T' && mapaResultado[e3.fila][e3.columna] != 'K') {
+			cerrados[e3.fila][e3.columna] = true;
 		}
 		else {
 			abiertos.push_back(e3);
 			e1.coste_f = g(e3) + h(e3);
 		}
 
-		if(mapaResultado[e4.fila][e2.columna] != 'S' && mapaResultado[e2.fila][e2.columna] != 'T') {
-			cerrados.push_back(e4);
+		if(mapaResultado[e4.fila][e4.columna] != 'S' && mapaResultado[e4.fila][e4.columna] != 'T' && mapaResultado[e4.fila][e4.columna] != 'K') {
+			cerrados[e4.fila][e4.columna] = true;
 		}
 		else {
 			abiertos.push_back(e4);
 			e1.coste_f = g(e4) + h(e4);
 		}
 
-		abiertos.sort;
-		actual = abiertos.front;
+		abiertos.sort(ordenar);
+		actual = abiertos.front();
 		ruta.push_back(actual);
 		
 	}
 
+	for (auto it = ruta.begin(); it != ruta.end(); ++it) {
+		cout << "Se ha creado la casilla " << it->fila << ", " << it->columna << endl;
+	}
+	
 	//Transformar lista en acciones y guardar en plan
+	list<estado>::const_iterator anterior, siguiente;
+
+	for (auto anterior = siguiente = ruta.cbegin(); *siguiente != destino; ++anterior) {
+		++siguiente;
+
+		if(anterior->fila < siguiente->fila) {
+			switch(anterior->orientacion) {
+				case 0:
+					plan.push_back(actTURN_L);
+					plan.push_back(actTURN_L);
+				break;
+
+				case 1:
+					plan.push_back(actTURN_R);
+				break;
+
+				case 2:
+				break;
+
+				case 3:
+					plan.push_back(actTURN_L);
+				break;
+			}
+		}
+
+		else if(anterior->fila > siguiente->fila) {
+			switch(anterior->orientacion) {
+				case 0:
+				break;
+
+				case 1:
+					plan.push_back(actTURN_L);
+				break;
+
+				case 2:
+					plan.push_back(actTURN_L);
+					plan.push_back(actTURN_L);
+				break;
+
+				case 3:
+					plan.push_back(actTURN_R);
+				break;
+			}
+		}
+
+		else if(anterior->columna < siguiente->columna) {
+			switch(anterior->orientacion) {
+				case 0:
+					plan.push_back(actTURN_R);
+				break;
+
+				case 1:
+				break;
+
+				case 2:
+					plan.push_back(actTURN_L);
+				break;
+
+				case 3:
+					plan.push_back(actTURN_L);
+					plan.push_back(actTURN_L);
+				break;
+			}
+		}
+
+		else {
+			switch(anterior->orientacion) {
+				case 0:
+					plan.push_back(actTURN_L);
+				break;
+
+				case 1:
+					plan.push_back(actTURN_L);
+					plan.push_back(actTURN_L);
+				break;
+
+				case 2:
+					plan.push_back(actTURN_R);
+				break;
+
+				case 3:
+				break;
+			}
+		}
+		
+		plan.push_back(actFORWARD);
+	}
+
+	return !ruta.empty();
 }
 
 Action ComportamientoJugador::think(Sensores sensores) {
